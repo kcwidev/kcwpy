94d5da77c8ad449e6342804cb69e52782a7badf7
Don Neill <neilljd@gmail.com>
2017-10-30 11:27:17 -0700

    added logic to detect what kind of illumination the observation contains

--- obs/Observation.py
+++ obs/Observation.py
@@ -38,7 +38,7 @@ class Observation:
     obsdir = None  # Original directory for observation
     hatpos = None  # hatch position: Open or Closed
     flimagin = None  # dome lamp imaging mode: on or off
-    flspectr= None  # dome lamp spectra mode: on or off
+    flspectr = None  # dome lamp spectra mode: on or off
     caltype = None  # calibration type: bias, dark, arc, etc.
     frameno = None  # image number
     skyobs = None  # sky observation? 0 - object, 1 - sky
@@ -58,6 +58,7 @@ class Observation:
     bnasnam = None  # Blue mask position name
     bnaspos = None  # Blue mask position
     shufrows = None  # Number of CCD rows shuffled
+    calmnam = None  # Cal mirror position name ('Sky', 'Mirror', 'Filter')
     calpnam = None  # Cal position name ('Sky', 'Polar', 'Lens')
     callang = None  # Cal polarizer angle
     ifunum = None  # Slicer number ( 0 -5, - 1 =unknown)
@@ -111,7 +112,7 @@ class Observation:
         else:
             self.telescope = 'Keck2'
         if 'DATEPCLR' in hdr:
-            self.date_obs = Time(hdr['DATEPCLR'],format='isot',
+            self.date_obs = Time(hdr['DATEPCLR'], format='isot',
                                  scale='utc')
         if 'TARGRA' in hdr and 'TARGDEC' in hdr:
             self.targ_coords = SkyCoord(hdr['TARGRA'], hdr['TARGDEC'],
@@ -119,3 +120,24 @@ class Observation:
         if 'RA' in hdr and 'DEC' in hdr:
             self.image_coords = SkyCoord(hdr['RA'], hdr['DEC'],
                                          unit=(u.hourangle, u.deg))
+        if 'HATPOS' in hdr:
+            self.hatpos = hdr['HATPOS']
+        if 'CALMNAM' in hdr:
+            self.calmnam = hdr['CALMNAM'].rstrip()
+        if 'Mirror' in self.calmnam:
+            self.illum = 'Cal'
+            if 'LMP0STAT' in hdr and 'LMP0SHST' in hdr:
+                if hdr['LMP0STAT'] and hdr['LMP0SHST']:
+                    self.illum = hdr['LMP0NAM'].rstrip()
+            if 'LMP1STAT' in hdr and 'LMP1SHST' in hdr:
+                if hdr['LMP1STAT'] and hdr['LMP1SHST']:
+                    self.illum = hdr['LMP1NAM'].rstrip()
+            if 'LMP3STAT' in hdr:
+                if hdr['LMP3STAT']:
+                    self.illum = hdr['LMP3NAM'].rstrip()
+        else:
+            if 'Open' in self.hatpos:
+                self.illum = 'Sky'
+            else:
+                self.illum = 'Test'
+
